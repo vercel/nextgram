49d7e2f506f20855bd6f05450a5fdcdbf9224b0b
Tim Neutkens <tim@timneutkens.nl>
2023-04-08 00:26:15 +0200

    Add metadataBase

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,9 @@
 import "../styles/reboot.css";
 
+export const metadata = {
+  metadataBase: new URL("https://nextgram.vercel.app"),
+};
+
 export default function Layout(props) {
   return (
     <html>
