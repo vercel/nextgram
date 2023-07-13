ae9e7ae4c0d3b00ddc896b59a54a8656c33f364e
Dmitry Nosov <nosov.sibers@gmail.com>
2023-07-13 17:50:08 +0200

    Remove useCallback and useRef

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,49 +1,56 @@
-'use client'
-import { useCallback, useRef, useEffect, MouseEventHandler } from 'react'
-import { useRouter } from 'next/navigation'
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Modal({ children }: { children: React.ReactNode }) {
-  const overlay = useRef(null)
-  const wrapper = useRef(null)
-  const router = useRouter()
+  const router = useRouter();
+  /* 
+  const overlay = useRef(null);
+  const wrapper = useRef(null);
+
 
   const onDismiss = useCallback(() => {
-    router.back()
-  }, [router])
+    router.back();
+  }, [router]);
 
   const onClick: MouseEventHandler = useCallback(
     (e) => {
       if (e.target === overlay.current || e.target === wrapper.current) {
-        if (onDismiss) onDismiss()
+        if (onDismiss) onDismiss();
       }
     },
     [onDismiss, overlay, wrapper]
-  )
+  );
 
   const onKeyDown = useCallback(
     (e: KeyboardEvent) => {
-      if (e.key === 'Escape') onDismiss()
+      if (e.key === "Escape") onDismiss();
     },
     [onDismiss]
-  )
+  );
+ */
+
+  function onKeyDown(e: KeyboardEvent) {
+    console.log(e.key);
+    if (e.key === "Escape") router.back();
+  }
 
   useEffect(() => {
-    document.addEventListener('keydown', onKeyDown)
-    return () => document.removeEventListener('keydown', onKeyDown)
-  }, [onKeyDown])
+    document.addEventListener("keydown", onKeyDown);
+    return () => document.removeEventListener("keydown", onKeyDown);
+  }, [onKeyDown]);
 
   return (
     <div
-      ref={overlay}
       className="fixed z-10 left-0 right-0 top-0 bottom-0 mx-auto bg-black/60"
-      onClick={onClick}
+      onClick={(e) => router.back()}
     >
       <div
-        ref={wrapper}
+        onClick={(e) => e.stopPropagation()}
         className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full sm:w-10/12 md:w-8/12 lg:w-1/2 p-6"
       >
         {children}
       </div>
     </div>
-  )
+  );
 }
