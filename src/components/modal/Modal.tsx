620574cc5e14f5d202c7df5922210ac82ce43bda
viraj  rai <69512213+viruop@users.noreply.github.com>
2023-07-31 16:17:25 +0530

    Update Modal.tsx
    fix modal image on desktop view

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -40,7 +40,7 @@ export default function Modal({ children }: { children: React.ReactNode }) {
     >
       <div
         ref={wrapper}
-        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full sm:w-10/12 md:w-8/12 lg:w-1/2 p-6"
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full sm:w-10/12 md:w-8/12 lg:w-1/2 xl:2/5 p-6"
       >
         {children}
       </div>
