fb8133c589066597a51a3ee6fb4a2aafb145be34
ra-kesh <iamrakeshkumar@pm.me>
2024-04-03 19:32:56 +0530

    feat:nested intercepted routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
-import Link from 'next/link';
+import Link from "next/link";
+import React from "react";
 
-export default function Page() {
-  let photos = Array.from({ length: 6 }, (_, i) => i + 1);
-
+const page = () => {
   return (
-    <section className="cards-container">
-      {photos.map((id) => (
-        <Link className="card" key={id} href={`/photos/${id}`} passHref>
-          {id}
-        </Link>
-      ))}
-    </section>
+    <Link href="/feed">
+      <button>Feed</button>
+    </Link>
   );
-}
+};
+
+export default page;
